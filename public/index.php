6fabf6fcd2ac21f96a3ee70ed833122b90791e21
djomla <mladen.martincevic@gmail.com>
2014-02-28 15:33:34 +0000

    NEW fixed configuration

--- public/index.php
+++ public/index.php
@@ -1,32 +1,17 @@
 <?php
+/**
+ * XPF
+ *
+ * @author      djomla <mladen.martincevic@g.com>
+ * @copyright   2014 djomla
+ * @link        http://github.com/mmartincevic/
+ * @version     1.0
+ * @package     XPF
+ *
+*/
+
 require '../vendor/autoload.php';
-
-// Prepare app
-$app = new \Slim\Slim(array(
-    'templates.path' => '../templates',
-    'mode'           => 'development',
-    'debug'          => true,
-    'environment'    => 'development'
-));
-
-// Create monolog logger and store logger in container as singleton 
-// (Singleton resources retrieve the same log resource definition each time)
-$app->container->singleton('log', function () {
-    $log = new \Monolog\Logger('slim-skeleton');
-    $log->pushHandler(new \Monolog\Handler\StreamHandler('../logs/app.log', \Monolog\Logger::DEBUG));
-    return $log;
-});
-
-// Prepare view
-$app->view(new \Slim\Views\Twig());
-$app->view->parserOptions = array(
-    'charset' => 'utf-8',
-    'cache' => realpath('../templates/cache'),
-    'auto_reload' => true,
-    'strict_variables' => false,
-    'autoescape' => true
-);
-$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+require '../config/config.php';
 
 // Define routes
 $app->get('/', function () use ($app) {
